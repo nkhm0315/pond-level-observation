c2ebedcd3cd464fef8cf52795a0b6c55a0aa1ba9
C.Nakahama <vcdj5812@yahoo.co.jp>
2023-02-03 10:50:13 +0900

    Create plo-master.py
    Created a program to acquire webcam images at regular intervals.

---
+++ plo-master.py
@@ -0,0 +1,15 @@
+import cv2
+import time
+
+cap = cv2.VideoCapture(0)
+timer = time.time()
+stopSec = 10
+
+while True:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    if timer + stopSec < time.time():
+        timer = time.time()
+        ret, source = cap.read()
+        cv2.imshow('Frame', source)
+        #cv2.imwrite('./data/' + str(time.time) + 'jpg', source)
